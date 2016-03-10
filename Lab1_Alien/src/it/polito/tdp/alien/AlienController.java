cff5726bfba6b77fd29c88ec7b7288b63ad73038
183346 <Robybe1@Roby>
2016-03-10 19:37:11 +0100

    modifica prova

--- Lab1_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab1_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
-	//
+	//test
     @FXML
     private ResourceBundle resources;
     @FXML
